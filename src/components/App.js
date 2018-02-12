f6e383fd13b6317c45b4a312f46493793f0a2ad5
alfredlam42 <alfred.lam.42@gmail.com>
2018-02-12 14:43:57 -0800

    layout complete - no functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
+// Custom Components
+import Header from './header/Header';
+import SignupForm from './signupForm/SignupForm';
+
+// Style
+import './App.css';
+
 export default class App extends Component {
   render(){
     return (
-      <div className="asdf">Hi</div>
+      <div className="signup-tab">
+        <Header />
+
+        <div className="signup-body">
+          <div className="signup-body-wrapper">
+            <div className="signup-title">Sign up for Team account</div>
+
+            <div className="signup-text">
+              Create an account to keep track of your customers, and contributors. Already have an account?&nbsp;
+              <a href="#">Log in here</a>
+            </div>
+
+            <SignupForm />
+          </div>
+        </div>
+      </div>
     )
   }
 }
